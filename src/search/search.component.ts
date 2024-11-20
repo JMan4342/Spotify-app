9594432e1d339a33284bfdf8bdf1941b72fe7b80
Joseph Manning <jgmanning3@yahoo.com>
2024-11-20 16:24:37 -0700

    Create service file for search component

--- src/search/search.component.ts
+++ src/search/search.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  artists: string[] = [];
 
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit() {
+    this.artists = this.searchService.getArtists();
+  }
 }
