e79be3e77945b5bb480f53d8ff3fa9dda50e779a
Joseph Manning <jgmanning3@yahoo.com>
2024-11-21 17:05:40 -0700

    Add user profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { SearchComponent } from '../search/search.component';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'search', component: SearchComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: UserProfileComponent },
 ];
