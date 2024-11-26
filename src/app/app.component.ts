46f319f5d89b1ef9ca5f698460d1985834eb8fec
Joseph Manning <jgmanning3@yahoo.com>
2024-11-26 16:31:03 -0700

    Install PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    RouterLink, 
+    RouterLinkActive
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
