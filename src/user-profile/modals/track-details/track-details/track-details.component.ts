e5ff8c4b695e348040d152030fe4695e25a6f170
Joseph Manning <jgmanning3@yahoo.com>
2024-12-07 09:51:32 -0700

    Build track detail modal

--- src/user-profile/modals/track-details/track-details/track-details.component.ts
+++ src/user-profile/modals/track-details/track-details/track-details.component.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+import { Track } from '../../../../shared/classes/track';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-track-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './track-details.component.html',
   styleUrl: './track-details.component.css'
 })
-export class TrackDetailsComponent {
+export class TrackDetailsComponent implements OnInit {
+  @Input() inTrackDetail = new Track;
+  @Output() outModalState = new EventEmitter<number>();
+  @Output() outModalVisible = new EventEmitter<boolean>();
 
+  ngOnInit(): void {
+    console.log(this.inTrackDetail);
+  }
+
+  closeModal(): void {
+    this.outModalState.emit(0);
+    this.outModalVisible.emit(false);
+  }
 }
--- src/user-profile/user-profile.component.ts
+++ src/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
@@ -17,12 +17,19 @@ import { TrackDetailsComponent } from './modals/track-details/track-details/trac
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule, CardModule, TableModule, DialogModule, TrackDetailsComponent],
+  imports: [
+    CommonModule,
+    CardModule,
+    TableModule,
+    DialogModule,
+    TrackDetailsComponent,
+  ],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrl: './user-profile.component.css',
 })
+export class UserProfileComponent implements OnChanges {
+  outTrackDetail: Track = new Track();
 
-export class UserProfileComponent {
   modalVisible: boolean = false;
   modalShown: number = 0;
   access_token: string = '';
@@ -32,47 +39,68 @@ export class UserProfileComponent {
   userTopArtists: Artist[] = [];
   tempArtist: Artist = new Artist();
 
-  constructor(private userProfileService: UserProfileService, private homeService: HomeService, private loginService: LoginService) {
+  constructor(
+    private userProfileService: UserProfileService,
+    private homeService: HomeService,
+    private loginService: LoginService
+  ) {
     this.access_token = this.loginService.accessToken$;
+  }
+  ngOnChanges(): void {
+    
   }
 
   ngOnInit() {
-    const spotifyUserProfile = this.userProfileService.getCurrentUserProfile(this.access_token);
-    const spotifyUserTopTracks = this.userProfileService.getCurrentUserTopTracks(this.access_token);
-    const spotifyUserTopArtists = this.userProfileService.getCurrentUserTopArtist(this.access_token);
+    const spotifyUserProfile = this.userProfileService.getCurrentUserProfile(
+      this.access_token
+    );
+    const spotifyUserTopTracks =
+      this.userProfileService.getCurrentUserTopTracks(this.access_token);
+    const spotifyUserTopArtists =
+      this.userProfileService.getCurrentUserTopArtist(this.access_token);
 
-    forkJoin([spotifyUserProfile, spotifyUserTopTracks, spotifyUserTopArtists]).subscribe({
-        next: results => {
-          console.log(results);
-          this.userProfile.DisplayName = results[0].display_name;
-          this.userProfile.Id = results[0].id;
-          this.userProfile.ImageUrl = results[0].images[0].url;
+    forkJoin([
+      spotifyUserProfile,
+      spotifyUserTopTracks,
+      spotifyUserTopArtists,
+    ]).subscribe({
+      next: (results) => {
+        console.log(results);
+        this.userProfile.DisplayName = results[0].display_name;
+        this.userProfile.Id = results[0].id;
+        this.userProfile.ImageUrl = results[0].images[0].url;
 
-          for (const i of results[1].items) {
-            let tempTrack = new Track();
-            tempTrack.Album = i.album.name;
-            tempTrack.AlbumId = i.album.id;
-            let tempImages = i.album.images.sort((a: { height: number; }, b: { height: number; }) => a.height - b.height);
-            tempTrack.AlbumImage = tempImages[0].url;
-            tempTrack.ArtistName = i.artists[0].name;
-            tempTrack.ArtistId = i.artists[0].id;
-            tempTrack.TrackName = i.name;
-            tempTrack.TrackId = i.id;
-            this.userTopTracks.push(tempTrack);
-          };
+        for (const i of results[1].items) {
+          let tempTrack = new Track();
+          tempTrack.Album = i.album.name;
+          tempTrack.AlbumId = i.album.id;
+          let tempImages = i.album.images.sort(
+            (a: { height: number }, b: { height: number }) =>
+              a.height - b.height
+          );
+          tempTrack.AlbumImage = tempImages[0].url;
+          tempTrack.ArtistName = i.artists[0].name;
+          tempTrack.ArtistId = i.artists[0].id;
+          tempTrack.TrackName = i.name;
+          tempTrack.TrackId = i.id;
+          this.userTopTracks.push(tempTrack);
+        }
 
-          for (const i of results[2].items) {
-            let tempArtist = new Artist();
-            tempArtist.ArtistName = i.name;
-            tempArtist.ArtistId = i.id;
-            tempArtist.Genres = i.genres;
-            let tempImages = i.images.sort((a: { height: number; }, b: { height: number; }) => a.height - b.height);
-            tempArtist.Image = tempImages[0].url;
-            this.userTopArtists.push(tempArtist);
-          };
-        },
-        error: err => console.log(err),
-      });
+        for (const i of results[2].items) {
+          let tempArtist = new Artist();
+          tempArtist.ArtistName = i.name;
+          tempArtist.ArtistId = i.id;
+          tempArtist.Genres = i.genres;
+          let tempImages = i.images.sort(
+            (a: { height: number }, b: { height: number }) =>
+              a.height - b.height
+          );
+          tempArtist.Image = tempImages[0].url;
+          this.userTopArtists.push(tempArtist);
+        }
+      },
+      error: (err) => console.log(err),
+    });
 
     // this.userProfileService.getCurrentUserProfile(this.access_token).subscribe({
     //   next: result => {
@@ -85,9 +113,16 @@ export class UserProfileComponent {
     //   error: err => console.log(err),
     // });
   }
-
-  onRowSelect(modal: number): void {
+  
+  onRowSelect(modal: number, event: any): void {
+    console.log(event);
     this.modalVisible = true;
     this.modalShown = modal;
+
+    if (modal == 1) {
+      this.outTrackDetail = new Track();
+      this.outTrackDetail = event.data;
+      // this.outTrackDetail.emit(tempTrackDetail);
+    };
   }
 }
