ab442fcf937b198174c06030ce18680132d3705a
Joseph Manning <jgmanning3@yahoo.com>
2024-12-11 15:22:05 -0700

    Setup spotify search api

--- src/search/search.component.ts
+++ src/search/search.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
 
 import { SearchService } from './search.service';
 import { HomeService } from '../home/home.service';
@@ -8,22 +13,44 @@ import { LoginService } from '../login/login.service';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent implements OnInit {
   artists: string[] = [];
   access_token: string = '';
+  searchTerm: string = '';
 
-  constructor(private searchService: SearchService, private homeService: HomeService, private loginService: LoginService) {
+  constructor(
+    private searchService: SearchService,
+    private homeService: HomeService,
+    private loginService: LoginService
+  ) {
     this.access_token = this.loginService.accessToken$;
   }
 
   ngOnInit() {
-    this.artists = this.searchService.getArtists();
-    this.searchService.searchArtist(this.access_token).subscribe({
-      next: result => console.log(result),
+    // this.artists = this.searchService.getArtists();
+    // this.searchService.searchArtist(this.access_token).subscribe({
+    //   next: (result) => console.log(result),
+    //   error: (err) => console.log(err),
+    // });
+  }
+
+  searchSpotify() {
+    this.searchService.searchSpotify(this.access_token, this.searchTerm).subscribe({
+      next: results => {
+        console.log(results);
+        
+      },
       error: err => console.log(err),
     });
   }
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -16,21 +16,21 @@ export class SearchService {
     return artists;
   }
 
-  searchArtist(access_token: string): Observable<any> {
+  searchSpotify(access_token: string, searchTerm: string): Observable<any> {
     // const baseHeaders = new HttpHeaders().set('Authorization', 'Bearer ' + access_token);
-    let authorization = '';
-    authorization = 'Authorization: Bearer ' + access_token;
+    // let authorization = '';
+    // authorization = 'Authorization: Bearer ' + access_token;
     // this.headers: {"Authorization": "Bearer " + access_token};
     return this.http
       .get(
-        'https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=album',
+        'https://api.spotify.com/v1/search?q=' + searchTerm + '&type=album%2Cartist%2Ctrack',
         {headers: {
           'Authorization': 'Bearer ' + access_token,
         }}
       )
       .pipe(
-        map((result) => {
-          return result;
+        map((results) => {
+          return results;
         })
       );
   }
